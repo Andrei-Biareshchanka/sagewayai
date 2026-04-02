782b7e1a8f860b1ac5bcc1fb87c75ff8d6a0f83d
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2026-04-02 07:54:54 +0200

    feat: scaffold client with React, Vite, and Tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
+  );
+}
+
+export { App };
+export default App;
